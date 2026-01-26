f674708ace18962863e78e68c766861b4fdef48b
Gustavo Araujo <guucarlos18@gmail.com>
2026-01-26 16:34:04 -0300

    First class position

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world");
+        Position position = new Position(2,3);
+        System.out.println(position);
     }
 }
